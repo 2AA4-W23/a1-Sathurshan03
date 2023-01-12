82568b123b0e51c6d0e3c2fb1751a39051ac0417
Sathurshan <arulmohs@mcmaster.ca>
2023-01-12 14:18:37 -0500

    Implemented F03: run 42 games in one simulation

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -5,29 +5,33 @@ public class PiratenKarpen {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Piraten Karpen Simulator!");
-        System.out.println("I'm rolling a dice");
-        Dice myDice [] = new Dice [8]; 
-        for (int i = 0; i < 8; i++)
-        {
-            myDice[i] = new Dice();
-            System.out.println(myDice[i].getRollValue());
-        }
 
-        //Check if turn ends
-        int numSkulls = 0;
-        for (int i = 0; i < 8; i++)
-        {
-            if (myDice[i].getRollValue().equals(Faces.SKULL)){
-                numSkulls++;
+        for (int i=0; i < 42; i++){
+            System.out.println("\nGAME " + (i+1));
+            System.out.println("I'm rolling a dice");
+            Dice myDice [] = new Dice [8]; 
+            for (int j = 0; j < 8; j++)
+            {
+                myDice[j] = new Dice();
+                System.out.println(myDice[j].getRollValue());
+            }
+
+            //Check if turn ends
+            int numSkulls = 0;
+            for (int j = 0; j < 8; j++)
+            {
+                if (myDice[j].getRollValue().equals(Faces.SKULL)){
+                    numSkulls++;
+                }
+            }
+
+            if (numSkulls >= 3)
+            {
+                System.out.println("Turn is over!");
             }
         }
 
-        if (numSkulls >= 3)
-        {
-            System.out.println("Turn is over!");
-        }
-
-        System.out.println("That's all folks!");
+        System.out.println("\nThat's all folks!");
     }
     
 }

77176a2973fb7c1b84c82ea7d5939fa8c05f344f
Sathurshan <arulmohs@mcmaster.ca>
2023-01-15 15:14:22 -0500

    Created a Simulation class to clean up PiratenKarpen class and to run mulitple simulations if needed

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -1,36 +1,11 @@
-import java.util.ArrayList;
-
-import pk.Game;
-import pk.Player;
+import pk.Simulation;
 
 public class PiratenKarpen {
 
     public static void main(String[] args) {
-        int totGames = 42;
-        int totPlayers = 2;
-        ArrayList <Player> players = new ArrayList<>();
         System.out.println("Welcome to Piraten Karpen Simulator!");
-
-        //Create the players
-        for (int i = 0; i < totPlayers; i++)
-        {
-            players.add(new Player("player" + (i+1)));
-        }
-
-        //Run the games
-        for (int i=0; i < totGames; i++){
-            new Game((i+1), players).runGame();
-        }
-
-        //Calculate and display the winnings data
-        System.out.println("=======Final Simulation Outcome=======");
-        double wins;
-        for(Player player:players)
-        {
-            wins = (double)player.getNumWins()/42*100;
-            System.out.printf("%s wins: %2.2f%%\n",player.getName() , wins);
-        }
-
+        Simulation simulation = new Simulation(42, 2);
+        simulation.runSimulation();
         System.out.println("\nThat's all folks!");
     }
     
---
+++ src/main/java/pk/Simulation.java
@@ -0,0 +1,42 @@
+package pk;
+
+import java.util.ArrayList;
+
+public class Simulation {
+    int totGames;
+    int totPlayers;
+
+    public Simulation(int totGames, int totPlayers){
+        this.totGames = totGames;
+        if (totPlayers >=2){
+            this.totPlayers = totPlayers;
+        }
+        else{
+            this.totPlayers = 2;
+        }
+    }
+
+    public void runSimulation()
+    {
+        //Create the players
+        ArrayList <Player> players = new ArrayList<>();
+        for (int i = 0; i < totPlayers; i++)
+        {
+            players.add(new Player("player" + (i+1)));
+        }
+
+        //Run the games
+        for (int i=0; i < totGames; i++){
+            new Game((i+1), players).runGame();
+        }
+
+        //Calculate and display the winnings data
+        System.out.println("=======Final Simulation Outcome=======");
+        double wins;
+        for(Player player:players)
+        {
+            wins = (double)player.getNumWins()/42*100;
+            System.out.printf("%s wins: %2.2f%%\n",player.getName() , wins);
+        }
+    }
+}

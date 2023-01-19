70c6cdd778675cc9403eed36d3c142f2683b01e6
Sathurshan <arulmohs@mcmaster.ca>
2023-01-19 11:40:14 -0500

    Changed the order of final results log statements for better logic

--- src/main/java/pk/Simulation.java
+++ src/main/java/pk/Simulation.java
@@ -33,16 +33,8 @@ public class Simulation {
             new Game((i+1), players).runGame();
         }
 
-        //Print each player's winnings to console
-        System.out.println("\n=======Final Simulation Outcome=======");
+        //log each player's winnings
         double wins;
-        for(Player player:players)
-        {
-            wins = (double)player.getNumWins()/42*100;
-            System.out.printf("%s wins: %2.2f%%\n",player.getName() , wins);
-        }
-
-        //Kepping log messages seperate so that it doesn't split the console output from above
         if("True".equals(System.getProperty("traceMode"))){
             LOG.trace("Final Simulation Outcome");
             for(Player player:players)
@@ -50,6 +42,14 @@ public class Simulation {
                 wins = (double)player.getNumWins()/42*100;
                 LOG.trace(String.format("%s wins: %2.2f%%",player.getName() , wins));
             }
+        }
+
+        //Print each player's winnings to console
+        System.out.println("\n=======Final Simulation Outcome=======");
+        for(Player player:players)
+        {
+            wins = (double)player.getNumWins()/42*100;
+            System.out.printf("%s wins: %2.2f%%\n",player.getName() , wins);
         }
     }
 }
